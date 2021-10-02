69292b3bb802be1b2479efd55be9d529301f45e7
Alex Moreira <alexlmoreira@outlook.com>
2021-10-02 09:36:45 -0300

    Update sh_zbx_server.bash

--- sh_zbx_server.bash
+++ sh_zbx_server.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Digite a senha do banco de dados: " SENHA
 read -p "Digite o IP do Zabbix Server: " SERVIDOR
-read -p "Digite o Hostname do Zabbix Proxy: " HOSTNAME
+read -p "Digite o Hostname do Zabbix : " HOSTNAME
 
 wget https://repo.zabbix.com/zabbix/4.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.4-1%2Bbionic_all.deb
 dpkg -i zabbix-release_4.*
@@ -13,7 +13,7 @@ Instalando aplicações
 
 "
 apt install mariadb-server zabbix-server-mysql zabbix-frontend-php \\
-zabbix-apache-conf zabbix-agent snmp snmp-mibs-downloader apache2 -y
+zabbix-apache-conf zabbix-agent snmp snmp-mibs-downloader apache2 php7.2* -y
 
 mv /etc/snmp/snmp.conf /etc/snmp/Old_snmp.conf
 echo "
@@ -23,6 +23,7 @@ echo "
 mibs +ALL
 mibAllowUnderline 1
 " > /etc/snmp/snmp.conf
+
 cp mibs_conectcor/* /usr/share/snmp/mibs/
 
 echo "Creating database zabbix"
@@ -35,13 +36,13 @@ echo "Grant permissions on tables"
 mysql -e "grant all privileges on zabbix.* to zabbix@localhost;"
 
 echo "Importing zabbix schema to DB"
-zcat /usr/share/doc/zabbix-server-mysql/schema.sql.gz | mysql -uzabbix -p$SENHA zabbix
+zcat /usr/share/zabbix-server-mysql/schema.sql.gz | mysql -uzabbix -p$SENHA zabbix
 
 
-sed -i "s@Server=127.0.0.1@Server=$SERVIDOR@g" /etc/zabbix/zabbix_proxy.conf
-sed -i "s@Hostname=Zabbix proxy@Hostname=$HOSTNAME@g" /etc/zabbix/zabbix_proxy.conf
-sed -i "s@DBUser=zabbix@DBUser="zabbix_proxy"@g" /etc/zabbix/zabbix_proxy.conf
-sed -i "s@# DBPassword=@ DBPassword=$SENHA@g" /etc/zabbix/zabbix_proxy.conf
+sed -i "s@Server=127.0.0.1@Server=$SERVIDOR@g" /etc/zabbix/zabbix_server.conf
+sed -i "s@Hostname=Zabbix server@Hostname=$HOSTNAME@g" /etc/zabbix/zabbix_server.conf
+sed -i "s@DBUser=zabbix@DBUser="zabbix_proxy"@g" /etc/zabbix/zabbix_server.conf
+sed -i "s@# DBPassword=@ DBPassword=$SENHA@g" /etc/zabbix/zabbix_server.conf
 
 
 systemctl enable zabbix-server.service
